cf4e9cb7abe9960aa704082f7df4e8acb141172c
eldelahoz <andelahoz13@gmail.com>
2024-09-29 17:55:10 -0500

    ADD: Componentes y edicion del main

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import Sign from "./components/Sign";
+import { Home } from "./api/Home";
+import Navbar from "./components/Navbar";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Sign />
+    <Navbar />
+    <Home />
   </StrictMode>
 );
