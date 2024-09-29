1984ad7fea9960185055a417b0a233993bf50fce
eldelahoz <andelahoz13@gmail.com>
2024-09-29 17:53:40 -0500

    ADD: Componente Home

---
+++ src/api/Home.jsx
@@ -0,0 +1,10 @@
+import { Container } from "../components/Container";
+import { Start } from "../components/Start";
+
+export const Home = () => {
+  return (
+    <Container>
+      <Start />
+    </Container>
+  );
+};
