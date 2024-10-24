5f700ab86bd330f054c77e77639ba7e8f4585767
Andres De la Hoz <54108311+eldelahoz@users.noreply.github.com>
2024-10-24 00:20:16 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,43 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+  use_cli_auth = true
+}
+
+resource "azurerm_resource_group" "existing" {
+  name     = "gestion_escolar"
+}
+
+resource "azurerm_container_registry" "imagenes_perso" {
+  name                = "miACRuniversidad"
+  resource_group_name = data.azurerm_resource_group.existing.name
+  location            = data.azurerm_resource_group.existing.location
+  sku                 = "Basic"
+}
+
+resource "null_resource" "docker_push" {
+  provisioner "local-exec" {
+    command = <<EOT
+      
+      az acr login --name ${azurerm_container_registry.imagenes_perso.name}
+      
+     
+      docker build -t ${azurerm_container_registry.imagenes_perso.login_server}/gestionescolar-front:latest .
+
+     
+      docker push ${azurerm_container_registry.imagenes_perso.login_server}/gestionescolar-front:latest
+    EOT
+  }
+
+  depends_on = [azurerm_container_registry.imagenes_perso]
+}
