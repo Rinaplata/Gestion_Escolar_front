fe7e3c94f767978370e8291197fe856c8b78f2cf
Andres De la Hoz <54108311+eldelahoz@users.noreply.github.com>
2024-10-24 11:22:41 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -73,16 +73,6 @@ resource "azurerm_container_group" "gestionescolar" {
 
   image_registry_credential {
     server   = azurerm_container_registry.imagenes_perso.login_server
-    username = azurerm_container_registry.imagenes_perso.admin_username
-    password = azurerm_container_registry.imagenes_perso.admin_password
   }
   depends_on = [azurerm_container_registry.imagenes_perso]
-}
-
-output "acr_username" {
-  value = azurerm_container_registry.imagenes_perso.admin_username
-}
-
-output "acr_password" {
-  value = azurerm_container_registry.imagenes_perso.admin_password
 }
