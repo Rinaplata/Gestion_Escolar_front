811d8be6b10b942b1f577cb50d9f44c8d73614c4
Rina Plata <55161289+Rinaplata@users.noreply.github.com>
2024-11-02 10:22:05 -0500

    Update index.js

--- src/services/index.js
+++ src/services/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://tendencias-g-escolar-back.vercel.app";
+const API_URL = "https://tendencias-g-escolar-back.vercel.app/api/";
 
 const api = axios.create({
   baseURL: API_URL,
